41697d0319f93aaa8e1de3d8150be72edead495d
Santosh Poudel <128451797+SantoshPoudel0@users.noreply.github.com>
2025-03-15 17:14:59 +0545

    Add client/src/components/layout/Navbar.js 1742038199622

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Box,
+  Avatar,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import { AccountCircle, Add as AddIcon } from '@mui/icons-material';
+import { useAuth } from '../../contexts/AuthContext';
+
+const Navbar = () => {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    logout();
+    handleClose();
+    navigate('/login');
+  };
+
+  const handleProfile = () => {
+    handleClose();
+    navigate(`/profile/${user?._id}`);
+  };
+
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <Typography
+          variant="h6"
+          component={RouterLink}
+          to="/"
+          sx={{
+            flexGrow: 1,
+            textDecoration: 'none',
+            color: 'inherit',
+            fontWeight: 700,
+          }}
+        >
+          Social Network
+        </Typography>
+
+        {user ? (
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+            <Button
+              component={RouterLink}
+              to="/create-post"
+              color="inherit"
+              startIcon={<AddIcon />}
+            >
+              Create Post
+            </Button>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              {user.profilePicture ? (
+                <Avatar src={user.profilePicture} alt={user.username} />
+              ) : (
+                <AccountCircle />
+              )}
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleProfile}>Profile</MenuItem>
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+          </Box>
+        ) : (
+          <Box>
+            <Button color="inherit" component={RouterLink} to="/login">
+              Login
+            </Button>
+            <Button color="inherit" component={RouterLink} to="/register">
+              Register
+            </Button>
+          </Box>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
