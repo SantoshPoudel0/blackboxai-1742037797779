f8954302602615e232f7b50de92458b44426385e
Santosh Poudel <128451797+SantoshPoudel0@users.noreply.github.com>
2025-03-15 17:08:42 +0545

    Add backend/models/User.js 1742037821705

---
+++ backend/models/User.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true,
+    trim: true,
+    minlength: [3, 'Username must be at least 3 characters long']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters long']
+  },
+  profilePicture: {
+    type: String,
+    default: 'default-avatar.png'
+  },
+  bio: {
+    type: String,
+    maxlength: [250, 'Bio cannot exceed 250 characters']
+  },
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  following: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+}, {
+  timestamps: true
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Method to compare password
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Method to get public profile (exclude sensitive data)
+userSchema.methods.toPublicProfile = function() {
+  const userObject = this.toObject();
+  delete userObject.password;
+  return userObject;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
